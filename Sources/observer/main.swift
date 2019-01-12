c207eedd5c34dbae88e751d9a4f646bcdf5294c7
Adam Sharp <adam@sharplet.me>
2019-01-12 09:11:39 -0500

    Log when a path is written, renamed or deleted

--- Sources/observer/main.swift
+++ Sources/observer/main.swift
@@ -1,1 +1,48 @@
-print("Hello, world!")
+import Darwin
+import Dispatch
+
+let paths = CommandLine.arguments.dropFirst()
+var sources: [DispatchSourceFileSystemObject] = []
+
+for path in paths {
+  guard case let fd = open(path, O_EVTONLY),
+    fd >= 0
+    else { perror(path); continue }
+
+  let source = DispatchSource.makeFileSystemObjectSource(
+    fileDescriptor: fd,
+    eventMask: [.delete, .rename, .write],
+    queue: .main
+  )
+
+  sources.append(source)
+
+  source.setCancelHandler {
+    close(fd)
+  }
+
+  var count = 0
+
+  source.setEventHandler {
+    let event: String
+
+    switch source.data {
+    case .delete:
+      event = "delete"
+    case .rename:
+      event = "rename"
+    case .write:
+      event = "write"
+    default:
+      return
+    }
+
+    count += 1
+
+    print("\(event): \(path) (\(count))")
+  }
+
+  source.resume()
+}
+
+dispatchMain()
